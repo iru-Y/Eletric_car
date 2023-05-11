aee926d92cf0d78d27ca2f128bf4fdb67504afde
iruy <yuri_matteus@hotmail.com>
2023-05-10 22:02:19 -0300

    Adicionando cardView

--- app/src/main/java/com/example/eletricCar/presentation/CalcularAutonomiaActivity.kt
+++ app/src/main/java/com/example/eletricCar/presentation/CalcularAutonomiaActivity.kt
@@ -1,18 +1,20 @@
 package com.example.eletricCar.presentation
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.eletriccar.R
 
 class CalcularAutonomiaActivity : AppCompatActivity() {
 
-    private lateinit var preco: EditText
+    private lateinit var price: EditText
     private lateinit var km: EditText
-    private lateinit var btnCalcular: Button
+    private lateinit var btnCalc: Button
+    private lateinit var tvResult: TextView
+    private lateinit var btnBack: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +24,24 @@ class CalcularAutonomiaActivity : AppCompatActivity() {
     }
 
     private fun setupView(){
-        this.preco = findViewById(R.id.et_preco_kwh)
+        this.price = findViewById(R.id.et_preco_kwh)
         this.km = findViewById(R.id.et_km_percorrido)
-        this.btnCalcular = findViewById(R.id.btn_calcular)
+        this.btnCalc = findViewById(R.id.btn_calcular)
+        this.tvResult = findViewById(R.id.ed_result)
+        this.btnBack = findViewById(R.id.iv_close_calc)
     }
-    private fun setupListeners(){
-        this.btnCalcular.setOnClickListener{
-           calc()
+    private fun setupListeners() {
+        this.btnCalc.setOnClickListener {
+            calc()
+        }
+        this.btnBack.setOnClickListener() {
+        finish()
         }
     }
     private fun calc(){
-        val textoDitado = preco.text.toString().toFloat() / km.text.toString().toFloat()
-        Log.d("Texto ditado ->", textoDitado.toString())
+        val price = this.price.text.toString().toFloat()
+        val km = this.km.text.toString().toFloat()
+        val result = price / km
+        tvResult.text = String.format("%.2f", result).replace(",", ".")
     }
 }
